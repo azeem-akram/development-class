8a0e50b6e3291917244e24ee22049bfbca50d856
Azeem Akram <azeemakram1997@gmail.com>
2024-03-05 04:02:25 +0500

    Blog Project Started

---
+++ Lecture26_Blog Project_Homepage, Database and Login, Register/includes/header.php
@@ -0,0 +1,76 @@
+<?php
+
+$results = $conn->query("Select * from categories");
+
+
+?>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+     <!-- me-auto: It left align the content -->
+     <!-- ms-auto: It Right align the content -->
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="login.php">Sign In</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="register.php">Register</a>
+        </li>
+       
+      </ul>
+      
+
+
+    </div>
+  </div>
+</nav>
+
+
+
+<div class="text-center m-2">
+    <img src="/assets/images/logo.png" width="100px"/>
+    <h2> My Blog </h2>
+</div>
+
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+
+        <?php 
+        if($results->num_rows > 0){
+            while($row = $results->fetch_assoc()){
+                ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><?php echo $row['name'] ?></a>
+                    </li>
+                <?php
+            }
+        }
+        ?>
+
+
+        
+        
+        
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
